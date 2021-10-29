2a1a936be1236c32d2944ce8a0e6ef759a1edff6
SalomeBrunon <s1704661@ed.ac.uk>
2021-10-29 15:30:13 +0100

    Question 3 done and files converted

--- Assignment1_script.sh
+++ Assignment1_script.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
-#QUESTION 2
-#Concatenate summary files into one big one
-echo "Concatenating fastqc summaries into one file"
+#QUESTION 3
+#Move to Tcongo_genome directory
+cd
+cd ~/Assignment1/AY21/Tcongo_genome; for gzip in *.gz; do gzip -d $gzip; done
 
-cd ~/Assignment1/AY21/fastq/fastqc_results
-cat */summary.txt > fastqc_summaries.txt 
+#Make database for bowtie2
+echo "Building Tcongolense_genome_database from its fasta file"
+bowtie2-build TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta Tcongolense_genome_database
 
-#Summary of FastQC
-echo "Number of raw sequences that passed one of the variables from fastqc:"
-grep -c PASS fastqc_summaries.txt
-#777
-echo "Number of raw sequences that had WARN for one of the variables from fastqc:"
-grep -c WARN fastqc_summaries.txt
-#86
-echo "Number of raw sequences that failed one of the variables from fastqc:"
-grep -c FAIL fastqc_summaries.txt
-#37
+#Read pairs alignment to T. congolense genome using bowtie2
+cd
+fqdir=Assignment1/AY21/fastq
+for x1 in "$fqdir"/*_1.fq; do
+    x2=${x1%_1.fq}_2.fq
+    if [[ -f $x2 ]] ; then
+        echo "Aligning $x1 and $x2 to Tcongolense_genome_database"
+        bowtie2 --threads 60 -x ~/Assignment1/AY21/Tcongo_genome/Tcongolense_genome_database -1 "$x1" -2 "$x2" -S "${x1%.*}"_output.sam
+    else
+        echo "$x2 not found" >&2
+    fi
+done
 
-#Per sequence quality score
-awk 'NR % 10 == 2' fastqc_summaries.txt > sequence_quality.txt
-echo "Number of raw sequences that passed the sequence quality test:"
-grep -c PASS sequence_quality.txt
+cd; mkdir Assignment1/AY21/genome_alignment
+mv  Assignment1/AY21/fastq/*.sam Assignment1/AY21/genome_alignment
 
+cd; cd Assignment1/AY21/genome_alignment
+
+#Convert output to indexed "bam" format with samtools
+for file in $(ls *.sam);
+do
+    # ${file%.*} means that it will take the file name and remove everything
+    # after the last punctuation in the name. 
+    echo "Converting $file to ${file%.*}.bam"
+    samtools view -bS $file > ${file%.*}.bam
+done
